3530a063312bee568617c311c7f2fe5c2c001452
barmud3 <130641348+barmud3@users.noreply.github.com>
2023-06-03 20:35:26 +0300

    Update ThreadSafeQueue.cpp

--- ThreadSafeQueue.cpp
+++ ThreadSafeQueue.cpp
@@ -1,11 +1,13 @@
 #include "ThreadSafeQueue.hpp"
+#include "ActiveObject.hpp"
+
+bool ThreadSafeQueue::stopFlag = false;
 
 void ThreadSafeQueue::Enqueue(void *item) {
 
     std::lock_guard<std::mutex> lock(mtx);  // Lock the mutex
 
     myQueue.push(item);                     // Add the item to the queue
-    cout << pthread_self() << " Pushing item "<< to_string(*((int*)item)) << " to queue" << endl;
     cv.notify_one();                        // Notify one waiting thread
 }
 
@@ -13,24 +15,24 @@ void* ThreadSafeQueue::Dequeue() {
     std::unique_lock<std::mutex> lock(mtx);
 
     // Check the stopFlag before waiting on the condition variable
-    if (stopFlag) {
+    if (ActiveObject::stopFlag) {
+        Stop();
         return nullptr;
     }
-    cout << pthread_self() << " check if the queue is filled, wait if not" << endl;
     cv.wait(lock, [this]{ return !myQueue.empty() || stopFlag; });
     
-    if (stopFlag) {
+    if (ActiveObject::stopFlag) {
+        Stop();
         return nullptr;
     }
 
     void* item = myQueue.front();
-    cout << pthread_self() << " dequeue item "<< to_string(*((int*)item)) << " from queue" << endl;
     myQueue.pop();
 
     return item;
 }
 
 void ThreadSafeQueue::Stop() {
-    stopFlag = true;
+    ThreadSafeQueue::stopFlag = true;
     cv.notify_all();
 }

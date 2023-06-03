ec98fe058a9521ffe49b030746e29c6146f5489b
barmud3 <130641348+barmud3@users.noreply.github.com>
2023-06-03 20:30:15 +0300

    Update ActiveObject.hpp

--- ActiveObject.hpp
+++ ActiveObject.hpp
@@ -1,3 +1,5 @@
+ao hpp 
+
 #ifndef OS_MATALA5_ACTIVEOBJECT_HPP
 #define OS_MATALA5_ACTIVEOBJECT_HPP
 
@@ -17,19 +19,25 @@ private:
     bool stopFlag;
 
 public:
+
     string name;
     std::function<void(void*)> myFunc;
     void CreateActiveObject(std::function<void(void*)> func , int OBNum , string name);
     bool isStopFlag() const;
-    ThreadSafeQueue* getQueue();
-    void stop();
+
     void joinThread();
     static void* Loop1(void* arg);
     static void* Loop2(void* arg);
     static void* Loop3(void* arg);
     static void* Loop4(void* arg);
     
+    friend ThreadSafeQueue* getQueue(ActiveObject* AO);
+    friend void stop(ActiveObject* AO);
 };
+
+void stop(ActiveObject* AO);
+ThreadSafeQueue* getQueue(ActiveObject* AO);
+
 
 
 
